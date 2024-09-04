be1d6c2929693d48d36962f0a1f92cb99dc088c0
rmoongit <emil_tt@mail.ru>
2024-09-04 22:16:09 +0300

    fix: fixed prettier & eslint formatter

--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="main">
-    <Field />
-    <Card />
-  </div>
+    <div class="main">
+        <Field />
+        <Card />
+    </div>
 </template>
 
 <script setup>
-import Field from "@/components/Field.vue";
-import Card from "@/components/Card.vue";
+import Field from '@/components/Field.vue'
+import Card from '@/components/Card.vue'
+
+import { ref } from 'vue'
 </script>
 
 <style scoped></style>
